77aea34bf8e8953f8e2babac2f5eb52a80a5d58f
Jeff <jefferson.vivas.95@gmail.com>
2026-05-19 14:47:51 +0200

    fix: add user avatar display in the settings dropdown

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,6 +29,7 @@
             <!-- Settings Dropdown -->
             @auth
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <img src="{{ Storage::url(Auth::user()->avatar) }}" alt="Avatar" class="w-10 h-10 rounded-full mb-2">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-emerald-800 focus:outline-none transition ease-in-out duration-150">
